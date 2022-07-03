0b18d06bc73a6bc92dcfb4e53c8ee1f9e72be102
Christiano112 <94331754+Christiano112@users.noreply.github.com>
2022-07-03 01:13:00 +0100

    fixed the styles

--- url-shortener_api.js
+++ url-shortener_api.js
@@ -1,34 +1,90 @@
 // POST API
-// var raw = "{\r\n    \"longUrl\":\"https://www.cbsnews.com/news/missing-details-investigation-48-hours/\"\r\n}";
 
-const form = document.getElementById("url-form");
+const formNew = document.getElementById("url-form");
 
-const preUrl = new FormData(form);
-const url = new URLSearchParams(preUrl);
+formNew.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-console.log([...url]);
+    // const preUrl = new FormData(formNew);
+    // const url = new URLSearchParams(preUrl);
 
-var requestOptions = {
-    method: 'POST',
-    body: url,
-    redirect: 'follow'
-};
+    // console.log([...url]);
 
-fetch("https://shorturl22.herokuapp.com/api/url/shorten", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    // var raw = "{\r\n    \"longUrl\":\"https://www.cbsnews.com/news/missing-idaho-kids-jj-vallow-tylee-ryan-lori-daybell-details-investigation-48-hours/\"\r\n}";
+    // let yourUrl = document.getElementById("lurl").value;
+    // var raw = "longUrl:yourUrl"
 
-let 
+    // let url = '"longUrl":"https://www.cbsnews.com/news/missing-idaho-kids-jj-vallow-tylee-ryan-lori-daybell-details-investigation-48-hours/"'
+
+    let inputUrl = document.getElementById("lurl").value;
+    // var longUrl = inputUrl;
+
+    
+    let url = {"longUrl": inputUrl}
+
+    var requestOptions = {
+        method: 'POST',
+        body: url,
+        redirect: 'follow'
+    };
+
+    fetch("https://shorturl22.herokuapp.com/api/url/shorten", requestOptions)
+        .then(function (res) {
+            console.log(res.status);
+            console.log(url)
+            // if (res.status === 404) {
+            //     window.open("notfound.html", target = "_blank");
+            // }
+
+            return res.text();
+        })
+
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 
 
-// GET API
+    // GET API
 // var requestOptions = {
 //     method: 'GET',
 //     redirect: 'follow'
 // };
 
 // fetch("shorturl22.herokuapp.com/dG71svLsL", requestOptions)
-//     .then(response => response.text())
+//     .then(function (res) {
+//         console.log(res.status);
+//         if (res.status === 404) {
+//             window.open("notfound.html", target = "_blank");
+//         }
+
+//         return res.text();
+//     })
+//     // .then(response => response.text())
 //     .then(result => console.log(result))
 //     .catch(error => console.log('error', error));
+
+})
+
+
+
+
+
+
+
+// .then(function (res) {
+//     console.log(res.status);
+//     if (res.status === 404) {
+//         alert(`User with email does not exist.`)
+//     } else if (res.status === 401) {
+//         alert(`Incorrect password, try again!`)
+//     } else {
+//         setTimeout(openNew, 2000)
+//         function openNew() {
+//             alert("Login is Successful");
+//             window.open("home.html", target = "_self")
+//         }
+//     }
+//     // if(!res.ok) {
+//     //     throw new Error("HTTP status " + res.status);
+//     // }
+//     return res.json();
+// })
